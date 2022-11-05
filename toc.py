d2ef9e4b4fac0a6048fd86aeaae770732de2bb3d
bighappysloth <113740714+bighappysloth@users.noreply.github.com>
2022-11-05 13:10:25 -0400

    furnished all features, added test pdfs

--- toc.py
+++ toc.py
@@ -2,10 +2,10 @@ import sys
 import platform
 import argparse
 import pathlib
+import io
 
 
-
-from PyPDF2 import PdfReader, PdfWriter
+from PyPDF2 import PdfReader, PdfWriter, PdfMerger, Transformation
 from decimal import Decimal
 
 parser = argparse.ArgumentParser()
@@ -18,17 +18,24 @@ parser = argparse.ArgumentParser()
 subparser = parser.add_subparsers(dest='command')
 subparser.required = True
 
+# Subparsers, three functions
 merge_parser = subparser.add_parser('merge')
 toc_parser = subparser.add_parser('toc')
 margin_parser = subparser.add_parser('margin')
 
 merge_parser.add_argument('-input', type=pathlib.Path, nargs='+', required=True, help='Input file path, required')
 merge_parser.add_argument('-output', type=str, nargs=1, required=False, help='Output file path (optional), default = first_input_file_M.pdf') #optional output argument
+merge_parser.add_argument('-inherit_toc', type=bool, nargs = 1, required=False, help='Inherit TOC from subfile')
+merge_parser.add_argument('-add_blank_page_after_subfile', '-add_blank', action='store_true', help='Add Blank Page after each subfile? (uses size of last page)')
 
+
+#TOC_Parser Arguments
 toc_parser.add_argument('-input', type=pathlib.Path, nargs=1, required=True, help='Input PDF path, required')
 toc_parser.add_argument('-config', type=pathlib.Path, nargs=1, required=True, help='Input config path, required')
 toc_parser.add_argument('-output', type=pathlib.Path, nargs=1, required=False, help='Output file path (optional)')
 
+
+# Margin Parser Arguments
 margin_parser.add_argument('-input', type=pathlib.Path, nargs='+', required=True, help='Input file path, required')
 margin_parser.add_argument('-margin_size', type=float, nargs=1, required=False, help='Absolute Horizontal Margin Size (optional), default = 180')
 margin_parser.add_argument('-z', type=float, nargs=1, required=False, help='z Factor (optional), default = 0.65')
@@ -40,14 +47,84 @@ margin_parser.add_argument('-scale', type=float, nargs=1, required=False, help='
 #finish the args thing
 args = parser.parse_args()
 
+def marginManager(path,margin,sf,z):
+	with open(path,'rb') as f:
+		p = PdfReader(f)
 
-def tocManager(z):
+		writer = PdfWriter()
+		path = str(path)
+		print('filename: ' + path)
+		print('printing p pages: '+str(p.pages))
+		print('0 page?:' + str(p.pages[0]))
+		for i in range(len(p.pages)):
+
+			page = p.pages[i]
+			if page==None:
+				print('EEEEE: ISNONE: ' + str(i))
+
+			w = float(page.mediabox.getWidth())
+			h = float(page.mediabox.getHeight())
+
+			print('\tpagenum: ' + str(i) + ' size: ' + str(w) + '/' + str(h) + '=' + str(w/h))
+			new_page = writer.addBlankPage(
+			w*sf + 2*margin,
+			h*sf + z*margin
+			)
+			translation = Transformation().translate(tx=float(margin),ty=float(z/2.)*margin)
+			page.add_transformation(translation)
+			new_page.merge_page(page,expand=False)
+			
+
+		fo_name = path[:-4] + "_E" + path[-4:] #attach _E at the end of the time, before extension, the file name is fixed and cannot be changed.
+
+		with open(fo_name,'wb') as fo:
+			writer.write(fo)
+
+# Require: List of Files, (array)
+# Name of Output
+# Add Blank Page between Files? (Bool)
+# Import Subfile Outilnes? (Bool)
+def mergeManager(list_of_files, output_name, add_blank_page_after_subfile,inherit_toc):
+
+	m = PdfMerger(strict = False)
+
+	print('Outputting to: ' + str(output_name))
+	for z in list_of_files: # List of files
+		with open(z,'rb') as f:    
+			print('Appending ' + str(z))
+			writer_to_flush = PdfWriter()
+			writer_to_flush.append_pages_from_reader(PdfReader(f, strict= False))
+			if add_blank_page_after_subfile:
+				writer_to_flush.add_blank_page() # Use the size of the last page
+			tmp = io.BytesIO()
+			writer_to_flush.write(tmp)
+			m.append(tmp, outline_item = z[:-4], import_outline=inherit_toc) #Add outline
+	with open(str(output_name),'wb') as fo:
+		m.write(fo)
+		print('Writing to ' + str(output_name))
+
+def printTocOutline(z):
+	tab_spacing = '\t'*z['indent_level']
+	print(tab_spacing + z['title'] + ' --> ' + str(z['pagenum'])+'\n')
+	if z.get('children'):
+		for c in z['children']:
+			printTocOutline(c)
+
+def recursiveToc(z,parent,parent_outline,writer):
+	z_params = {'title': z['title'], 'pagenum': z['pagenum'], 'parent': parent_outline} 
+	z_outline = writer.add_outline_item(**z_params) # Top level
+	if z.get('children'):
+		for c in z['children']:
+			recursiveToc(c,z,z_outline,writer)
+	return z_outline
+
+def tocManager(z,toc_structure):
 
 	# Output File Name can be Modified
-	if args.o: 
-		oname = str(o) 
-	else: 
-		oname = z[:-4]+str('_TOC')+z[-4:]
+	# if args.o: 
+	# 	oname = str(o) 
+	# else: 
+	oname = z[:-4]+str('_TOC')+z[-4:]
 	print('tocManager outputting to :' + oname)
 
 	with open(z,'rb') as f:
@@ -58,7 +135,8 @@ def tocManager(z):
 
 		r_outline = reader.outline
 		print('Outline Length: ' + str(len(reader.outline)))
-		print('Outline Entry Keys: ' + str(reader.outline[0].keys()))
+		if(len(reader.outline)!=0):
+			print('Outline Entry Keys: ' + str(reader.outline[0].keys()))
 
 		#add all the magic pages
 		writer.clone_document_from_reader(reader)
@@ -68,12 +146,19 @@ def tocManager(z):
 
 		#print('get_named_dest_root():' + str(writer.get_named_dest_root()))
 		
-		print('get_outline_root():' + writer.get_outline_root())
-		x = {'title': 'Page 2','pagenum': 2,'parent': writer.get_outline_root()}
+		print('get_outline_root():' + str(writer.get_outline_root()))
+		outline_root = writer.get_outline_root()
+		for y in toc_structure:
+			y_params = {'title': y['title'], 'pagenum': y['pagenum'], 'parent': outline_root} 
+			y_outline = writer.add_outline_item(**y_params) # Top level
+			if y.get('children'):
+				for c in y['children']:
+					recursiveToc(c,y,y_outline,writer)
+		writer.page_mode = "/UseOutlines"
+		with open(oname, "wb") as fp:
+		 	writer.write(fp)
+
 		
-		writer.add_outline_item(*x)
-		#with open(oname, "wb") as fp:
-		# 	writer.write(fp)
 
 
 
@@ -101,7 +186,7 @@ if __name__ == '__main__':
 				# {'pagenum': 5, 'title': 'Chapter 1.2.1', 'indent_level': 2}
 				# {'pagenum': 7, 'title': 'Chapter 1.3', 'indent_level': 1}
 				# {'pagenum': 12, 'title': 'Chapter 2', 'indent_level': 0}
-				print(lines[i])
+				#print(lines[i])
 			
 			#Now we have a valid 'lines' list of dictionaries.
 			#Traverse it like a stack structure.
@@ -109,30 +194,148 @@ if __name__ == '__main__':
 			temp_stack = []
 
 			for i in range(0,len(lines)):
+				previous_indent_level = len(temp_stack)-1 #keeps track of hierarchy
 				current_indent_level = lines[i]['indent_level']
-				if i==0: 
-					previous_indent_level = 0
-				else:
-					previous_indent_level = lines[i-1]['indent_level']
+				# print('current/prev indent_level' + str(current_indent_level)+'/'+str(previous_indent_level))
 				
 				if current_indent_level==previous_indent_level:
 					# If the temp_stack is not empty then we pop the last one,
 					# config_outline_structure keeps track of indent_level 0 entries.
-					if len(temp_stack) !=0:
-						config_outline_structure(temp_stack.pop())
+					if previous_indent_level==0: # if on the same indent level, and if indent level is root for example [Chapter 1], meets 'Chapter 2'
+						config_outline_structure.append(temp_stack.pop()) #pop to root directory.
+						
+					else:
+						# [Chapter 1, Chapter 1.1] meets 'Chapter 1.2'.
+						# pop Chapter 1.1, and append Chapter 1.2 to Chapter 1,
+						# then append Chapter 1.2 to the temp_stack in anticipation for Chapter 1.2.1
 
-					lines[i]=lines.del('indent_level') # no need to keep track of this
-					config_outline_structure.append(lines[i])
-				elif current_indent_level>= 1 and current_indent_level > previous_indent_level:
+						temp_stack.pop() # pop the brother,
+						
+						#This part fixes the last entry, as in 'Chapter 1'
+						last_entry = temp_stack.pop()
+
+						if last_entry.get('children'):
+							last_entry['children'].append(lines[i])
+						else:
+							last_entry['children'] = [lines[i]]
+						temp_stack.append(last_entry)
+
+					# always append to temp_stack the current line.
+					temp_stack.append(lines[i])
+					#print(temp_stack)
+					
+				elif current_indent_level > previous_indent_level:
 					# Handling the case where it goes into a subchapter.
 					# There has to be an existing previous line or else it won't work. We must check that the first line cannot be indented in the beginning too.
+
+					if previous_indent_level==-1: #pushing the first element.
+						temp_stack.append(lines[i])
+						# print('Pushing First Element')
+						# print(temp_stack)
+					else:
+						#Modification of the last entry.
+						last_entry = temp_stack.pop()
+						if last_entry.get('children'):
+							last_entry['children'].append(lines[i])
+						else:
+							last_entry['children'] = [lines[i]]
+						temp_stack.append(last_entry)
+						temp_stack.append(lines[i])
+						# print(temp_stack)
+
+				else:
+					# Now we have to pop enough entries within the temp_stack.
+					while current_indent_level < previous_indent_level:
 					
+						temp_entry = temp_stack.pop()
+						if temp_entry['indent_level']==0:
+							config_outline_structure.append(temp_entry) #only append to config_outline_structure if and only if it belongs to the root directory.
+						previous_indent_level=len(temp_stack)-1
+					if current_indent_level==previous_indent_level:
+					# If the temp_stack is not empty then we pop the last one,
+					# config_outline_structure keeps track of indent_level 0 entries.
+						if previous_indent_level==0: # if on the same indent level, and if indent level is root for example [Chapter 1], meets 'Chapter 2'
+							config_outline_structure.append(temp_stack.pop()) #pop to root directory.
+							
+						else:
+							# [Chapter 1, Chapter 1.1] meets 'Chapter 1.2'.
+							# pop Chapter 1.1, and append Chapter 1.2 to Chapter 1,
+							# then append Chapter 1.2 to the temp_stack in anticipation for Chapter 1.2.1
 
+							temp_stack.pop() # pop the brother,
+							
+							#This part fixes the last entry, as in 'Chapter 1'
+							last_entry = temp_stack.pop()
 
+							if last_entry.get('children'):
+								last_entry['children'].append(lines[i])
+							else:
+								last_entry['children'] = [lines[i]]
+							temp_stack.append(last_entry)
 
+						# always append to temp_stack the current line.
+						
+						#print(temp_stack)
+						
+					elif current_indent_level > previous_indent_level:
+						# Handling the case where it goes into a subchapter.
+						# There has to be an existing previous line or else it won't work. We must check that the first line cannot be indented in the beginning too.
 
+						if previous_indent_level==-1: #pushing the first element.
+							temp_stack.append(lines[i])
+							# print('Pushing First Element')
+							# print(temp_stack)
+						else:
+							#Modification of the last entry.
+							last_entry = temp_stack.pop()
+							if last_entry.get('children'):
+								last_entry['children'].append(lines[i])
+							else:
+								last_entry['children'] = [lines[i]]
+							temp_stack.append(last_entry)
+					temp_stack.append(lines[i])
+							# print(temp_stack)
+					
+				# print(temp_stack)
+			while len(temp_stack)!=0:
+				last_entry = temp_stack.pop()
+				if (last_entry['indent_level']==0):
+					config_outline_structure.append(last_entry)
 				#implement logic here
-				print(current_indent_level)
+			for x in config_outline_structure:
+				printTocOutline(x)
+			tocManager(str(args.input[0]),config_outline_structure)
 
-	#tocManager(fname)
+
+#def mergeManager(list_of_files, output_name, add_blank_pages_between_files,import_file_outlines):
+	elif args.command=='merge':
+		# generate default file name FIRSTFILE_M.pdf
+
+		valid_files = []
+		for z in args.input: #admit valid files only
+			if str(z)[-4:]=='.pdf' and str(z)[-6:]!='_M.pdf':
+				valid_files.append(str(z))
+		default_output_name = sorted(valid_files)[0]
+		default_output_name = default_output_name[:-4] + str('_M') + default_output_name[-4:]
+
+		merge_args = {'list_of_files': valid_files, 'output_name': default_output_name, 'add_blank_page_after_subfile': args.add_blank_page_after_subfile, 'inherit_toc': args.inherit_toc}
+		
+		if args.output:
+			merge_args['output_name'] = args.output[0]
+		mergeManager(**merge_args)
+	elif args.command=='margin':
+
+		#defaults
+		default_margin = 180
+		default_z = 0.65
+		default_sf = 1.0
+		if args.margin_size: 
+			default_margin = args.margin_size
+		if args.z: 
+			default_z = args.z
+		if args.scale: 
+			default_sf = args.scale
+		for x in args.input:
+			if str(x)[-4:]=='.pdf' and str(x)[-6:]!='_E.pdf':
+				marginManager(x,default_margin,default_sf,default_z)
 	print('Terminating Program...')
